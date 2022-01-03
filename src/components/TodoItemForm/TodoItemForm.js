0ac37041a1c7f7f64e7a27bd866b8560c934736e
ikonuk <28847139+ibrahimkonuk@users.noreply.github.com>
2022-01-03 14:53:06 +0100

    Update to-do list form component
    Add form and form elements to create a basic to-do list item

--- src/components/TodoItemForm/TodoItemForm.js
+++ src/components/TodoItemForm/TodoItemForm.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './TodoItemForm.module.css';
 
-const TodoItemForm = () => (
-  <div className={styles.TodoItemForm} data-testid="TodoItemForm">
-    TodoItemForm Component
-  </div>
-);
+const TodoItemForm = () => {
+  const [inputValue, setInputValue] = useState('');
+  return (
+    <div className={styles.TodoItemForm} data-testid="TodoItemForm">
+      <input className='todo-form__input' type='text' value={inputValue}
+        onChange={e => setInputValue(e.target.value)} />
+      <button className='todo-form__button'>Add</button>
+    </div>
+  )
+};
 
 TodoItemForm.propTypes = {};
 
