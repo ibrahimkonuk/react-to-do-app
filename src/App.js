f88e812a7f2ff6af95369d9aa08d18976328a801
ikonuk <28847139+ibrahimkonuk@users.noreply.github.com>
2022-01-03 14:58:28 +0100

    Add to-do list component to the app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import TodoList from './components/TodoList/TodoList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>React to-do app</h1>
       </header>
+      <nav></nav>
+      <main>
+        <TodoList />
+      </main>
+      <footer></footer>
     </div>
   );
 }
