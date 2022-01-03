5394b07e8c07565f25b5baef2effc22423dc8f72
ikonuk <28847139+ibrahimkonuk@users.noreply.github.com>
2022-01-03 14:50:51 +0100

    Update to-do list item component
    To-do list item component updated to dynamically display the item in the list.

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -2,10 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './TodoListItem.module.css';
 
-const TodoListItem = () => (
-  <div className={styles.TodoListItem} data-testid="TodoListItem">
-    TodoListItem Component
-  </div>
+const TodoListItem = ({ todo }) => (
+  <article className={styles.TodoListItem} data-testid="TodoListItem">
+    <h1 className='todo-item__title'> {todo.title} </h1>
+    <p className='todo-item_description'> {todo.description} </p>
+    <button className='todo-item__completed'>Mark as completed</button>
+    <button className='todo-item__remove'>Remove</button>
+  </article>
 );
 
 TodoListItem.propTypes = {};
