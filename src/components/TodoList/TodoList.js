0835cb5eb4723e4d35c67d5402f8f9fcf6faa72d
ikonuk <28847139+ibrahimkonuk@users.noreply.github.com>
2022-01-03 14:57:35 +0100

    Update to-do list component
    Add to-do list mock data and to-do list form to the component

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './TodoList.module.css';
+import TodoItem from '../TodoListItem/TodoListItem'
+import TodoItemForm from '../TodoItemForm/TodoItemForm';
 
-const TodoList = () => (
+const TodoList = ({ todos = [{ title: 'To-do item', description: 'To-do item description.' }] }) => (
   <div className={styles.TodoList} data-testid="TodoList">
-    TodoList Component
+    <TodoItemForm />
+    {todos.map((todo, index) => < TodoItem key={index} todo={todo} />)}
   </div>
 );
 
