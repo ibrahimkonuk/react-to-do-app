acd060d6e6e54cc950c3b033e0a84fa80349bd89
ikonuk <28847139+ibrahimkonuk@users.noreply.github.com>
2022-01-06 14:57:55 +0100

    Update UI of the to-do list component #6

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -5,25 +5,41 @@ import TodoListItem from '../TodoListItem/TodoListItem'
 import TodoItemForm from '../TodoItemForm/TodoItemForm';
 import { loadTodos, removeTodoRequest, updateTodoRequest } from '../thunks';
 
-const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed, isLoading, startLoadingTodos }) => {
+import Box from '@mui/material/Box';
+import CircularProgress from '@mui/material/CircularProgress';
+
+
+const TodoList = ({
+  todos = [], onRemovePressed, onCompletedPressed, isLoading, startLoadingTodos
+}) => {
+
   useEffect(() => {
     startLoadingTodos();
   }, []);
 
-  const loadingMessage = <div>Loading todos...</div>;
+  const loadingMessage = <CircularProgress />;
+
   const content = (
-    <div className={styles.TodoList} data-testid="TodoList">
+    <>
       <TodoItemForm />
-      {todos.map(todo =>
-        <TodoListItem key={todo.id} todo={todo}
-          onRemovePressed={onRemovePressed}
-          onCompletedPressed={onCompletedPressed} />
-      )}
-    </div>
+      <Box
+        className={styles.TodoList}
+        data-testid="TodoList"
+        sx={{ justifyContent: 'space-around', flexWrap: 'wrap', display: 'flex', flexDirection: 'row', p: 1, m: 1, bgcolor: 'background.paper' }}>
+        {
+          todos.map(todo =>
+            <TodoListItem key={todo.id} todo={todo}
+              onRemovePressed={onRemovePressed}
+              onCompletedPressed={onCompletedPressed} />
+          )
+        }
+      </Box >
+    </>
   );
   return isLoading ? loadingMessage : content;
 
 }
+
 const mapStateToProps = state => ({
   isLoading: state.isLoading,
   todos: state.todos,
