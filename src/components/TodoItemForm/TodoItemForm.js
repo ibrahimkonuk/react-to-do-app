cad026b933d5f32198a773483a9a41bd1e20adb8
ikonuk <28847139+ibrahimkonuk@users.noreply.github.com>
2022-01-06 14:58:11 +0100

    Update UI of the to-do list form component #6

--- src/components/TodoItemForm/TodoItemForm.js
+++ src/components/TodoItemForm/TodoItemForm.js
@@ -3,34 +3,101 @@ import { connect } from 'react-redux';
 import styles from './TodoItemForm.module.css';
 import { addTodoRequest } from '../thunks';
 
+import TextField from '@mui/material/TextField';
+import SaveIcon from '@mui/icons-material/Save';
+import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import Container from '@mui/material/Container';
+
 
 const TodoItemForm = ({ todos, onCreatePressed }) => {
 
   const [inputTitle, setTitleValue] = useState('');
   const [inputDescription, setDecriptionValue] = useState('');
+  const [open, setOpen] = React.useState(false);
+  const handleClick = () => {
+    setOpen(true);
+  };
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
 
   return (
-    <div className={styles.TodoItemForm} data-testid="TodoItemForm">
-      <label>Title</label>
-      <input className='todo-form__title' type='text' value={inputTitle}
-        onChange={e => setTitleValue(e.target.value)} />
-      <label>Description</label>
-      <input className='todo-form__description' type='text' value={inputDescription}
-        onChange={e => setDecriptionValue(e.target.value)} />
-      <button
-        className='todo-form__button'
-        onClick={() => {
-          const isDuplicateText =
-            todos.some(todo => todo.title === inputTitle);
-          if (!isDuplicateText && inputTitle && inputDescription) {
-            onCreatePressed(inputTitle, inputDescription);
-            setTitleValue('');
-            setDecriptionValue('');
-          }
-        }}
-      >Add</button>
-    </div >
+    <>
+      <Container
+        data-testid="TodoItemForm"
+        className={styles.TodoItemForm}
+        sx={{ m: 2, mx: "auto", display: 'flex', flexDirection: 'column' }}
+        component="form"
+        maxWidth='sm'
+      >
+
+        <Typography
+          sx={{ mx: "auto", pb: 3 }}
+          component="div"
+          variant="h4">
+          Create a new to-do
+        </Typography>
+
+        <TextField
+          required
+          id="outlined-required"
+          className='todo-form__title'
+          sx={{ mb: 2 }}
+          label="Title"
+          type='text'
+          value={inputTitle}
+          onChange={e => setTitleValue(e.target.value)}
+        />
+
+        <TextField
+          required
+          id="outlined-multiline-static"
+          className='todo-form__description'
+          sx={{ mb: 2 }}
+          label="Description"
+          multiline
+          rows={3}
+          type='text'
+          value={inputDescription}
+          onChange={e => setDecriptionValue(e.target.value)}
+        />
+
+        <Button
+          className='todo-form__button'
+          sx={{ color: "#f50057", maxWidth: 100, mx: "auto" }}
+          size="large"
+          startIcon={<SaveIcon />}
+          onClick={() => {
+            const isDuplicateText =
+              todos.some(todo => todo.title === inputTitle);
+            if (!isDuplicateText && inputTitle && inputDescription) {
+              onCreatePressed(inputTitle, inputDescription);
+              handleClick();
+              setTitleValue('');
+              setDecriptionValue('');
+            }
+          }}>
+          Save
+        </Button>
+
+        <Snackbar
+          open={open}
+          autoHideDuration={4000}
+          onClose={handleClose}
+          message="Note created"
+        />
+
+      </Container>
+      <Divider />
+    </>
   )
+
 };
 
 const mapStateToProps = state => ({
